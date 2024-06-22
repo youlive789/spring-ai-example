96784ba4c19706d976d7c3e5a07549cc53352160
youlive789 <youlive789@gmail.com>
2024-06-22 13:05:42 +0900

    feat: stream testing

--- src/main/java/com/example/chatbot/service/ai/NameAgentService.java
+++ src/main/java/com/example/chatbot/service/ai/NameAgentService.java
@@ -1,11 +1,20 @@
 package com.example.chatbot.service.ai;
 
+import java.time.Duration;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
 
 @Service
 @RequiredArgsConstructor
@@ -16,13 +25,20 @@ public class NameAgentService {
     private final ChatClient chatClient;
 
     public String askName(String message) {
-        return chatClient.prompt()
+        final List<String> action = chatClient.prompt()
             .user(userSpec -> userSpec
                 .text(prompt)
                 .param("query", message)
             )
-            .call()
-            .content();
+            .stream()
+            .content()
+            .takeUntil(res -> res.contains("getName"))
+            .collectList()
+            .block();
+
+        System.out.println(action.toString());
+
+        return null;
     }
 
     private String getName() {
--- src/main/java/com/example/chatbot/service/ai/OllamaConfig.java
+++ src/main/java/com/example/chatbot/service/ai/OllamaConfig.java
@@ -1,5 +1,10 @@
 package com.example.chatbot.service.ai;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
